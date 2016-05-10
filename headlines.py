4c146babd8c999464b5dea21614cc1b8e6b2e50a
Noxyo <sgodondrugs@gmail.com>
2016-05-10 05:10:59 +0100

    Weather, Currency & News - Flask App

--- headlines.py
+++ headlines.py
@@ -13,8 +13,14 @@ from flask import request
 
 app = Flask(__name__)
 
+# OpenWeatherMap API Key
+WEATHER_KEY = "e950e0ad047e4aeb3d44f682902483ae"
+# OpenExchangeRates API Key
+EXCHANGE_KEY = "615c7a4f4d8a45d3894ac75b22cd257e"
 
+# API Key Url
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=e950e0ad047e4aeb3d44f682902483ae"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=615c7a4f4d8a45d3894ac75b22cd257e"
 
 RSS_FEEDS = {'qdance': 'http://podcast.q-dance.nl/audio/q-dancepodcast.xml',
 			 'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -22,7 +28,9 @@ RSS_FEEDS = {'qdance': 'http://podcast.q-dance.nl/audio/q-dancepodcast.xml',
 			 'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
 DEFAULTS = {'publication': 'qdance',
-			'city': 'Braga,PT'}
+			'city': 'Braga,PT',
+			'currency_from':'USD',
+			'currency_to':'EUR'}
 
 
 @app.route("/")
@@ -39,7 +47,20 @@ def home():
 	if not city:
 		city = DEFAULTS['city']
 	weather = get_weather(city)
-	return render_template("home.html", articles=articles,weather=weather)
+	# get customized currency based on user input or default
+	currency_from =  request.args.get("currency_from")
+	if not currency_from:
+		currency_from = DEFAULTS['currency_from']
+	currency_to = request.args.get("currency_to")
+	if not currency_to:
+		currency_to = DEFAULTS['currency_to']
+	rate, currencies = get_rate(currency_from, currency_to)
+	return render_template("home.html", articles=articles,
+										weather=weather,
+										currency_from=currency_from,
+										currency_to=currency_to,
+										rate=rate,
+										currencies=sorted(currencies))
 
 def get_news(query):
 	if not query or query.lower() not in RSS_FEEDS:
@@ -50,7 +71,6 @@ def get_news(query):
 	return feed['entries']
 
 def get_weather(query):
-	#api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=e950e0ad047e4aeb3d44f682902483ae"
 	query = urllib.quote(query)
 	url = WEATHER_URL.format(query)
 	data = urllib2.urlopen(url).read()
@@ -64,6 +84,13 @@ def get_weather(query):
 					'country': parsed['sys']['country']
 				  }
 	return weather	
+
+def get_rate(frm, to):
+	all_currency =  urllib2.urlopen(CURRENCY_URL).read()
+	parsed = json.loads(all_currency).get('rates')
+	frm_rate = parsed.get(frm.upper())
+	to_rate = parsed.get(to.upper())
+	return (to_rate/frm_rate, parsed.keys())
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
